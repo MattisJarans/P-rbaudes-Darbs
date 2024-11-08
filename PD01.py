3c52a6a786936d8739b0c6c84ddb5dfa01d18baa
MattisJarans <matiss.jarans@gmail.com>
2024-11-08 11:44:20 +0200

    Add files via upload

---
+++ PD01.py
@@ -0,0 +1,42 @@
+integer_var = 100;
+float_var = 65, 5;
+string_var = "Programmesana"
+print("Vesels skaitlis:", integr_var);
+print("Decimālskaitlis", float_var);
+print("Teksts", string_var);
+
+x = int(input('Enter x:50 '))
+
+y = int(input('Enter y:25 '))
+print('x or y = int')
+x1 = x + y
+x2 = x - y
+x3 = x * y
+x4 = x / y
+print(x1)
+print(x2)
+print(x3)
+print(x4)
+
+num = float(input("0"))
+
+if num > 0:
+    print("Skaitlis ir pozitivs")
+elif num < 0:
+    print("Skaitlis ir negativs")
+  else:
+      print("Skaitlis ir 0")
+      
+try:
+    atzime >= 9;
+    limenis = "A"
+ elif: atzime >= 7;
+     limenis = "B"
+     elif: atzime >=5;
+     limenis = "C"
+     elif: atzime >=3;
+     limenis = "D"
+     elif: atzime >=1;
+     limenis = "E"
+ elif: atzime >= 0;
+     limenis = "F"
